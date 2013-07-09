e359231b03f4d83b90ec5b6ec369e4fe85067863
Ubuntu <ubuntu@ip-172-31-32-64.us-west-2.compute.internal>
2013-07-09 22:01:17 +0000

    Updated to read msg from file index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+#!/usr/bin/env node
+var fs=require('fs');
+var infile = "index.html";
+
+var message = new Buffer(fs.readFileSync(infile));
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(message.toString("utf-8", 0, 30))
 });
 
 var port = process.env.PORT || 5000;
